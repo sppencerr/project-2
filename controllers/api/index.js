23bbbb1ded983962995b667b9ff5dbfb52e5b3e8
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-10-31 19:03:06 -0600

    testing

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoutes');
+// const userRoutes = require('./userRoutes');
+// const postRoutes = require('./postRoutes');
 const homeRoutes = require('./homeRoutes');
 const catRoutes = require('./catRoutes');
 
-router.use('/users', userRoutes);
-router.use('/projects', postRoutes);
+// router.use('/users', userRoutes);
+// router.use('/projects', postRoutes);
 router.use('/api', apiRoutes);
 router.use('/cats',catRoutes);
 
