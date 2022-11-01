2d5e51f77a2b6939c0a498d0ab3dd42610c91ad8
Spencer Gerritsen <spencergerritsen@gmail.com>
2022-10-31 19:11:01 -0600

    ROUTES

--- controllers/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const routesMain = require('./routesMain');
-const apiRoutes = require('./api/');
-
-router.use('/', routesMain);
-router.use('/api', apiRoutes);
-
-module.exports = router;
