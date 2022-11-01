9d1f29bf3347b12a12affe2ced6b520482709f1d
Benjamin Sokolik <sokolik_b@hotmail.com>
2022-10-31 18:32:21 -0600

    small changes to /controllers/index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,9 +4,9 @@ const router = require('express').Router();
 // !Commented out for troubleshooting server launch
 // !Once DB is ready, uncomment 6-10
 // const userRoutes = require('./userRoutes');
-// const postRoutes = require('./postRoutes');
+// const postRoutes = require('./noteRoutes');
 
-// router.use('/users', userRoutes);
-// router.use('/projects', postRoutes);
+// router.use('./userRoutes', userRoutes);
+// router.use('./noteRoutes', noteRoutes);
 
 module.exports = router;
